8ecfbd22e0251d98e46036f715d17d9bf2c16390
Sam Ennis <sam.e.ennis@gmail.com>
2025-05-28 14:58:34 -0400

    Initial Commit

---
+++ ShieldBug/ShieldBugApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShieldBugApp.swift
+//  ShieldBug
+//
+//  Created by Sam Ennis on 5/28/25.
+//
+
+import SwiftUI
+
+@main
+struct ShieldBugApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
